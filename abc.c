f621e0efab6225ef3a4bb47692dbe55b6dc07213
arjun tp <arjun.tp@medibox.in>
2017-03-11 22:43:12 +0530

    first commit

---
+++ abc.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+void main(){
+int i;
+for(i=5;i>=0;i--)
+printf("\n%d\n",i);
+}
+
+
+
+
+
+/*int n,rev=0; 
+printf("Enter a number to reverse\n");
+scanf("%d", &n);
+while (n!= 0)
+{
+rev=rev*10;
+rev=rev+n%10;
+n=n/10;
+} 
+printf("Reverse of entered number is = %d\n", rev);
+}
+*/
